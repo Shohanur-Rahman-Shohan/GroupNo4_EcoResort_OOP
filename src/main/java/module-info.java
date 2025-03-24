2100b874a3e0dcd1b42ec123f2b8fd0e18aa7e18
Shohanur Rahman <shohanurrahma345@gmail.com>
2025-03-25 00:41:58 +0600

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.example.groupno4_ecoresort_oop {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.example.groupno4_ecoresort_oop to javafx.fxml;
+    exports org.example.groupno4_ecoresort_oop;
+}
