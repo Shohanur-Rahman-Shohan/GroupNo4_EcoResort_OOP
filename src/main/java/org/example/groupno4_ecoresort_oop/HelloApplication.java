bd06caedb797663852c503539cb2d601e44648d7
Shohanur Rahman <shohanurrahma345@gmail.com>
2025-04-04 18:47:02 +0600

    No Idea Where If I Am On The Right Track Or Not

--- src/main/java/org/example/groupno4_ecoresort_oop/HelloApplication.java
+++ src/main/java/org/example/groupno4_ecoresort_oop/HelloApplication.java
@@ -4,16 +4,31 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.example.groupno4_ecoresort_oop.shohan.Manager;
+import org.example.groupno4_ecoresort_oop.shohan.generalManager;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+
+        ArrayList<Manager> managers= new ArrayList<>();
+        Manager GM= new generalManager(2312227, "Shohanur", "Rahman", "shohanur@gmail.com", "12345", "+8801xxxxxxxxx");
+        managers.add(GM);
+
+        SceneSwitcher.stage= stage;
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                HelloApplication.class.getResource("LoginView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("ECO Resort");
         stage.setScene(scene);
+
+
+
         stage.show();
     }
 
--- src/main/java/org/example/groupno4_ecoresort_oop/shohanur/TestController.java
+++
@@ -1,7 +0,0 @@
-package org.example.groupno4_ecoresort_oop.shohanur;
-
-public class TestController
-{
-    @javafx.fxml.FXML
-    public void initialize() {
-    }}
