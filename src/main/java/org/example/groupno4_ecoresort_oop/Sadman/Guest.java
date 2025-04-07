cf4aa35c538d69081ba78899e4e362297cc81469
Sadman_2230824 <2230824@iub.edu.bd>
2025-04-07 12:04:39 +0600

    init comm

--- src/main/java/org/example/groupno4_ecoresort_oop/Sadman/Guest.java
+++
@@ -1,4 +0,0 @@
-package org.example.groupno4_ecoresort_oop.Sadman;
-
-public class Guest {
-}
---
+++ src/main/java/org/example/groupno4_ecoresort_oop/Sadman/guestController.java
@@ -0,0 +1,7 @@
+package org.example.groupno4_ecoresort_oop.Sadman;
+
+public class guestController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
