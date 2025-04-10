b9810043be84ec4bb20179df9118a32a7098b621
Arman-zz <armanuddin1010@gmail.com>
2025-04-10 21:37:20 +0600

    created Dashboards in SceneBuilder

--- src/main/java/org/example/groupno4_ecoresort_oop/Arman/controller/ACDashboardController.java
+++ src/main/java/org/example/groupno4_ecoresort_oop/Arman/controller/ACDashboardController.java
@@ -4,4 +4,37 @@ public class ACDashboardController
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void trackGuestParticipationButtonOnClick(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void modifyExistingTourButtonOnClick(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void feedbackAndSuccessRateButtonOnClick(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void scheduleEcoTourButtonOnClick(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void manageBudgetSportsButtonOnClick(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void updateTourAndPriceButtonOnClick(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void resolveComplaintsButtonOnClick(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void assignTourGuideButtonOnClick(Event event) {
+    }
+}
