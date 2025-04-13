5a7943ac6f42baa90236f835b4b33207a954d21b
Sadman_2230824 <2230824@iub.edu.bd>
2025-04-14 02:11:03 +0600

    init comm

--- src/main/java/org/example/groupno4_ecoresort_oop/Sadman/guestController.java
+++
@@ -1,7 +0,0 @@
-package org.example.groupno4_ecoresort_oop.Sadman;
-
-public class guestController
-{
-    @javafx.fxml.FXML
-    public void initialize() {
-    }}
---
+++ src/main/java/org/example/groupno4_ecoresort_oop/Sadman/guestDashboard.java
@@ -0,0 +1,30 @@
+package org.example.groupno4_ecoresort_oop.Sadman;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class guestDashboard
+{
+    @javafx.fxml.FXML
+    private Label guestLabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void modifyReservationOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void avaialbleRoomsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void feedbackOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void MakeReservationOnAction(ActionEvent actionEvent) {
+    }
+}
