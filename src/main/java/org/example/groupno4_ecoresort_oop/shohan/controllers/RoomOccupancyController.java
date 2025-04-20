4ba2c72e383b923bf61997d81841f19f98653ac9
Shohanur Rahman <shohanurrahma345@gmail.com>
2025-04-20 08:22:28 +0600

    ..

--- src/main/java/org/example/groupno4_ecoresort_oop/shohan/controllers/RoomOccupancyController.java
+++ src/main/java/org/example/groupno4_ecoresort_oop/shohan/controllers/RoomOccupancyController.java
@@ -1,7 +1,53 @@
 package org.example.groupno4_ecoresort_oop.shohan.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import org.example.groupno4_ecoresort_oop.SceneSwitcher;
+
 public class RoomOccupancyController
 {
     @javafx.fxml.FXML
+    private TableColumn guestName_TC;
+    @javafx.fxml.FXML
+    private TableView roomOccupancy_TV;
+    @javafx.fxml.FXML
+    private TableColumn guestID_TC;
+    @javafx.fxml.FXML
+    private TableColumn stayDuration_TC;
+    @javafx.fxml.FXML
+    private TableColumn serialNo_TC;
+    @javafx.fxml.FXML
+    private ComboBox<String> filterAs_CB;
+    @javafx.fxml.FXML
+    private TableColumn roomStatus_TC;
+    @javafx.fxml.FXML
+    private TableColumn guestCatagory_TC;
+    @javafx.fxml.FXML
+    private TableColumn reservationDate_TC;
+    @javafx.fxml.FXML
+    private Label message_L;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void backButtonOnAction(ActionEvent actionEvent) {
+        try {
+            SceneSwitcher.switchTo("user/DashBoard");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @javafx.fxml.FXML
+    public void resetFilter_OA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void filter_OA(ActionEvent actionEvent) {
+    }
+}
